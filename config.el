4c5ad60ac1fb63e7d8dbfc680a797d2a4f4791e8
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-25 23:41:45 +0530

    feat: add beacon mode and adjusting font size for gui

--- config.el
+++ config.el
@@ -1,6 +1,8 @@
+(beacon-mode 1)
+
 (setq doom-theme 'doom-one)
 
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13 :weight 'regular))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 15 :weight 'regular))
 
 (setq display-line-numbers-type 'relative)
 
