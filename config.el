247397cf9bb572db57c793f61ec76133ba362bd6
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-25 23:30:17 +0530

    feat: initial commit

---
+++ config.el
@@ -0,0 +1,7 @@
+(setq doom-theme 'doom-one)
+
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13 :weight 'regular))
+
+(setq display-line-numbers-type 'relative)
+
+(setq org-directory "~/org/")
